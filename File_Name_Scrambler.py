a819a76bc002fa660137e6f84535d04c555c6a60
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2019-08-17 22:38:54 +0930

    Update File_Name_Scrambler.py

--- File_Name_Scrambler.py
+++ File_Name_Scrambler.py
@@ -1,6 +1,7 @@
 """ File_Name_Scrambler.
 
 A simple python script that renames a specified file with a randomly generated name and file extension.
+Operations conducted through a command line.
 
 """
 
